9d3d0b390269ef238d84d06a782bc3592d2ed77a
ichebadu <chukwu.iche@gmail.com>
2022-12-01 07:20:44 -0800

    modified

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,11 @@
-able File  11 lines (11 sloc)  305 Bytes
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 15 == 0:
-            print("FizzBuzz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
+        if (i % 15 == 0):
+            print("FizzBuzz ", end="")
+        elif (i % 3 == 0):
+            print("Fizz ", end="")
+        elif (i % 5 == 0):
+            print("Buzz ", end="")
         else:
-            print("{:d}".format(i), end=" ")
+            print("{:d} ".format(i), end="")
