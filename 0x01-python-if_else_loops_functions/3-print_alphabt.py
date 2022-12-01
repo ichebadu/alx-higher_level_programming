3620ae1118a9a94cf75e164d7c4c47a42be1d11e
ichebadu <chukwu.iche@gmail.com>
2022-12-01 07:11:12 -0800

    modified

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for a in range(97, 123):
-    print('{:c}'.format(a), end='')
+for i in 'abcdefghijklmnopqrstuvwxyz':
+    if (i != "q" and i != "e"):
+        print("{}".format(i), end="")
