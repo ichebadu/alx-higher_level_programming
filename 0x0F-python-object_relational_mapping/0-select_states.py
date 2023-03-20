2a68ea1ebe8699cc6c0d66d075fd49ff9be3d200
ichebadu <chukwu.iche@gmail.com>
2023-03-19 21:16:06 -0700

    Write a script that lists all states from the database hbtn_0e_0_usa:

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,10 +7,10 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states")
-    rows = cursor.fetchall()
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states")
+    rows = curs.fetchall()
     for row in rows:
         print(row)
-    cursor.close()
+    curs.close()
     db.close()
