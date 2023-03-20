15e0b1550782d4c9417649dfb5092873ef87e020
ichebadu <chukwu.iche@gmail.com>
2023-03-19 21:56:58 -0700

    rationals mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     curs = db.cursor()
-    match = sys.argv[4]
-    curs.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    curs.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
     rows = curs.fetchall()
     for row in rows:
         print(row)
