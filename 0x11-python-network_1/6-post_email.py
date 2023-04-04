7e558c0983c128da23850b4a64d0de0d0113a3af
ichebadu <chukwu.iche@gmail.com>
2023-04-03 22:48:25 -0700

    0x11. Python - Network #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a script that takes a URL as input, sends a 
+request to that URL, and displays the value of the X-Request-Id
+header variable in the response.
+"""
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
