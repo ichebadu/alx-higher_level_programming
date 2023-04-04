2e88e67bb5f278a093f702945a478183fca2ec49
ichebadu <chukwu.iche@gmail.com>
2023-04-04 09:41:08 -0700

    updated

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,23 +2,17 @@
 """Displays the top 10 recent commits for a GitHub repository
 specified as an argument.
 """
-import sys
 import requests
+import sys
 
+if __name__ == '__main__':
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
+    response = requests.get(url)
+    commits = response.json()
 
-if __name__ == "__main__":
-    owner = sys.argv[2]
-    repo = sys.argv[1]
-    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-
-    try:
-        r = requests.get(url)
-        r.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(f"Error: {err}")
-    else:
-        commits = r.json()
-        for commit in commits[:10]:
-            sha = commit.get("sha")
-            author = commit.get("commit").get("author").get("name")
-            print(f"{sha}: {author}")
+    for commit in commits[:10]:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f'{sha}: {author_name}')
